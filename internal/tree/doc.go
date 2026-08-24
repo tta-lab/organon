a4d327bedf3a30e87f17eb02224fa89b95cf8ed8
Neil <bn0010100@gmail.com>
2026-08-24 13:06:33 +0800

    chore(web): update remaining doc references after removal

--- internal/tree/doc.go
+++ internal/tree/doc.go
@@ -1,5 +1,5 @@
 // Package tree provides a generic box-drawing tree renderer.
-// Used by both src (symbol trees) and url (heading trees).
+// Used by both src (symbol trees) and markdown (heading trees).
 //
 // Plane: shared
 package tree
